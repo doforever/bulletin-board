cc62f42005e44572ca119ddd4eedd273ae596295
Dorota Tomala <doforever@gmail.com>
2021-05-21 15:51:41 +0200

    Add local state to add post component

--- src/components/views/PostAdd/PostAdd.js
+++ src/components/views/PostAdd/PostAdd.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
 import clsx from 'clsx';
@@ -19,15 +19,50 @@ import Grid from '@material-ui/core/Grid';
 import styles from './PostAdd.module.scss';
 
 const Component = ({className, children, user}) => {
+  const [newPost, changeNewPost] = useState({
+    title: '',
+    text: '',
+    price: null,
+    tel: '',
+    address: '',
+    photo: '',
+  });
+
+  const changeHandler = e => {
+    changeNewPost({ ...newPost, [e.target.name]: e.target.value });
+  };
+
   if (!user) return <NotFound />;
   else { return (
     <Grid className={clsx(className, styles.root)} container spacing={2} justify='center'>
       <Paper className={styles.paper}>
         <Grid item>
           <form noValidate autoComplete='off' className={styles.form}>
-            <TextField id="title" label="Title" variant="outlined" fullWidth margin='normal'/>
-            <TextField id="text" label="Text" variant="outlined" multiline rows={3} fullWidth margin='normal'/>
             <TextField
+              value={newPost.title}
+              onChange={changeHandler}
+              name='title'
+              id="title"
+              label="Title"
+              variant="outlined"
+              fullWidth
+              margin='normal'
+            />
+            <TextField
+              value={newPost.text}
+              onChange={changeHandler}
+              name='text'
+              id="text"
+              label="Text"
+              variant="outlined"
+              multiline
+              rows={3}
+              fullWidth
+              margin='normal'
+            />
+            <TextField
+              value={newPost.price}
+              onChange={changeHandler}
               margin='normal'
               label="Price"
               variant="outlined"
@@ -39,11 +74,34 @@ const Component = ({className, children, user}) => {
                 inputComponent: NumberFormatCustom,
               }}
             />
-            <TextField id="tel" label="Tel" variant="outlined" fullWidth type='tel' margin='normal'/>
-            <TextField id="address" label="Address" variant="outlined" multiline rows={2} fullWidth margin='normal'/>
+            <TextField
+              value={newPost.tel}
+              onChange={changeHandler}
+              name='tel'
+              id="tel"
+              label="Tel"
+              variant="outlined"
+              fullWidth type='tel'
+              margin='normal'
+            />
+            <TextField
+              value={newPost.address}
+              onChange={changeHandler}
+              name='address'
+              id="address"
+              label="Address"
+              variant="outlined"
+              multiline
+              rows={2}
+              fullWidth
+              margin='normal'
+            />
             <FormControl variant="outlined" margin='normal'>
               <Button htmlFor='photo' color='primary' variant='contained' component='label'>Add photo</Button>
               <input
+                value={newPost.photo}
+                onChange={changeHandler}
+                name='photo'
                 id="photo"
                 type="file"
                 style={{'display': 'none'}}
