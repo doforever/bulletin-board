a055c98cbad547988da6a8b402da31b7b1d9d268
Dorota Tomala <doforever@gmail.com>
2021-06-01 16:51:21 +0200

    Include variables in add post code

--- src/redux/postsRedux.js
+++ src/redux/postsRedux.js
@@ -64,7 +64,6 @@ export const loadOneRequest = id => {
 
 export const savePostRequest = (postData, accessToken) => {
   return async dispatch => {
-    console.log(accessToken);
     dispatch(startRequest('SAVE_POST'));
     try {
       const res = await axios.post(`${API_URL}/api/posts`, postData, {
