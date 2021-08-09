b1ca5f683651cb3951c032371cafc98818cfbe0e
Dorota Tomala <doforever@gmail.com>
2021-08-09 13:52:21 +0200

    Configure remote db connection

--- backend/config.js
+++ backend/config.js
@@ -1,5 +1,5 @@
 const dbURI = process.env.NODE_ENV === 'production'
-  ? 'mongodb://localhost:27017/bulletinBoard'
+  ? `mongodb+srv://doforever:${process.env.dbpass}@cluster0.tvjci.mongodb.net/BulletinBoard?retryWrites=true&w=majority`
   : 'mongodb://localhost:27017/bulletinBoard';
 
 module.exports = {dbURI};
