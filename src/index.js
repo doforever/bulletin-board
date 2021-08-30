33811eb676f6f71b8fb85ffdf579c5b217d0f41f
Dorota Tomala <doforever@gmail.com>
2021-08-30 11:43:09 +0200

    Comply with PWA standard

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { App } from './App';
-import * as serviceWorker from './serviceWorker';
+import reportWebVitals from './reportWebVitals';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration.js';
 import { Auth0Provider } from '@auth0/auth0-react';
 import { audience } from './config';
 
@@ -18,7 +19,5 @@ ReactDOM.render(<Auth0Provider
   <App />
 </Auth0Provider>, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorkerRegistration.register();
+reportWebVitals();
